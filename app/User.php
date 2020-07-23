bb6695f2abd262273bad9a87911f0f5d20d2ef2b
Michael Umfahrer <michael@umfahrer.dev>
2020-07-23 12:16:05 +0200

    add user roles and admin middleware

--- app/User.php
+++ app/User.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -11,12 +10,19 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * USER ROLES
+     */
+    const ROLE_ADMIN = 'ADMIN';
+    const ROLE_STUDENT = 'STUDENT';
+    const ROLE_TEACHER = 'TEACHER';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role'
     ];
 
     /**
@@ -36,4 +42,31 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Check if current User has Role ADMIN
+     *
+     * @return bool
+     */
+    public function isAdmin() {
+        return $this->role == self::ROLE_ADMIN;
+    }
+
+    /**
+     * Check if current User has Role TEACHER
+     *
+     * @return bool
+     */
+    public function isTeacher() {
+        return $this->role == self::ROLE_TEACHER;
+    }
+
+    /**
+     * Check if current User has Role STUDENT
+     *
+     * @return bool
+     */
+    public function isStudent() {
+        return $this->role == self::ROLE_STUDENT;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['hasAdminRole'])->group(function(){
+    Route::get('/admin', 'AdminController@index')->name('admin');
+});
